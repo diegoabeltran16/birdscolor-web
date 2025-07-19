93c49dcece3d88de9ac1d0fbdf03bdeb2b928878
Diego Alejandro Beltran <diegobeltran1016@hotmail.com>
2025-07-19 15:08:53 -0500

    styles y simbiosis esp updt

--- src/pages/simbiosis_es.js
+++ src/pages/simbiosis_es.js
@@ -1,8 +1,24 @@
 // File: src/pages/simbiosis_es.js
 import Head from 'next/head'
 import Script from 'next/script'
+import { useRouter } from 'next/router'
 
 export default function SimbiosisEs() {
+  const router = useRouter()
+
+  // Opcional: función para mostrar panel/modal en vez de redirigir
+  const handleClick = (target) => {
+    if (target === 'cerebro') {
+      router.push('/cerebro')
+      // o mostrar panel/modal aquí
+    } else if (target === 'cuerpo') {
+      router.push('/cuerpo')
+    } else if (target === 'comunidad') {
+      router.push('/comunidad')
+      // o hacer scroll a sección
+    }
+  }
+
   return (
     <>
       <Head>
@@ -19,19 +35,25 @@ export default function SimbiosisEs() {
         {/* Círculo superior izquierdo: Cerebro */}
         <div className="venn-item" id="circle-left">
           <div className="outer-ring ring-left" />
-          <div className="venn-circle spin-left">🧠</div>
+          <div className="venn-circle" tabIndex={0} role="button">
+            <span>🧠</span>
+          </div>
           <div className="venn-label label-top cloud-bubble">Cerebro</div>
         </div>
         {/* Círculo superior derecho: Cuerpo */}
         <div className="venn-item" id="circle-right">
           <div className="outer-ring ring-right" />
-          <div className="venn-circle spin-right">💪</div>
+          <div className="venn-circle" tabIndex={0} role="button">
+            <span>💪</span>
+          </div>
           <div className="venn-label label-top cloud-bubble">Cuerpo</div>
         </div>
         {/* Círculo inferior centrado: Comunidad */}
         <div className="venn-item" id="circle-bottom">
-          <div className="outer-ring ring-left" />
-          <div className="venn-circle heartbeat">🌍</div>
+          <div className="outer-ring" />
+          <div className="venn-circle" tabIndex={0} role="button">
+            <span>🌍</span>
+          </div>
           <div className="venn-label label-bottom">Comunidad</div>
         </div>
       </div>
